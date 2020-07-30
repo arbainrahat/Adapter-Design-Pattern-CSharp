d5cb7be2885eb1678faefe132e54e89d82cdcc7f
arbainrahat <68120270+arbainrahat@users.noreply.github.com>
2020-07-30 21:38:44 +0500

    Add files via upload

---
+++ Adapter/VlcPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter
+{
+    class VlcPlayer : AdvanceMediaPlayer
+    {
+        public void playVlc(string fileName)
+        {
+            Console.WriteLine("Playing Vlc file . Name "+fileName);
+        }
+        public void playMp4(string fileName)
+        {
+
+        }
+    }
+}
