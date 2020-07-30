01a5229a439f70bf9fb0a60f9bbd0e6275a9df57
arbainrahat <68120270+arbainrahat@users.noreply.github.com>
2020-07-30 21:37:13 +0500

    Add files via upload

---
+++ Adapter/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //AdvanceMediaPlayer ad = new VlcPlayer();
+            //ad.playVlc("music");
+
+            AudioPlayer aud = new AudioPlayer();
+            aud.play("mp3","Allah Ho.mp3");
+            aud.play("mp4", "Pakistan.mp4");
+            aud.play("vlc", "Ye Zammen.vlc");
+            aud.play("avi", "Ho.avi");
+
+            Console.ReadLine();
+        }
+    }
+}
